90ba00b3f0ddf6e10995baa57b222fee341a947d
KwanWaiPang <gwpscut@163.com>
2023-04-26 20:11:32 +0800

    comment

--- driver_code/dv-ros-master/dv_ros_capture/include/dv_ros_capture/parameters_loader.hpp
+++ driver_code/dv-ros-master/dv_ros_capture/include/dv_ros_capture/parameters_loader.hpp
@@ -9,20 +9,20 @@ namespace dv_ros_node {
  * Struct containing the parameters
  */
 struct Params {
-	int64_t timeIncrement = 1000;
+	int64_t timeIncrement = 1000;//事件的触发频率(Increment of the timestamp at each iteration of the thread. The thread sleeps for timeIncrement micros) (原本为1000，即1KHZ)
 	bool frames           = true;//是否有图像
 	bool events           = true;//是否有事件
 	bool imu              = true;//是否有IMU
-	bool triggers         = true;
+	bool triggers         = true;//If true, Trigger msg containing Trigger data is published
 	std::string cameraName;//相机名称（带序列号的）
-	std::filesystem::path aedat4FilePath;
+	std::filesystem::path aedat4FilePath;//Path to the aedat4 file, if not specified the first available camera will be read
 	std::filesystem::path cameraCalibrationFilePath;
-	std::string cameraFrameName    = "camera";
-	std::string imuFrameName       = "imu";
+	std::string cameraFrameName    = "dvs";//(原本为camera)Camera reference frame name for publishing IMU extrinsic calibration
+	std::string imuFrameName       = "imu";// IMU reference frame name for publishing IMU extrinsic calibration
 	bool transformImuToCameraFrame = false;//是否将IMU数据转换到相机坐标系下 (原本为true)
-	bool unbiasedImuData           = true;
+	bool unbiasedImuData           = false;//(原本为true)If true, publish unbiased IMU data, that is IMU bias values that are read from calibration file will be subtracted out of the raw IMU stream
 	bool noiseFiltering            = true;//是否进行噪声滤波（原本为false）
-	int64_t noiseBATime            = 2000;//默认的去除噪声的参数
+	int64_t noiseBATime            = 2000;//默认的去除噪声的参数(Noise filter background activity time threshold in microseconds)
 
 // A list of other cameras connected with synchronization cable to this camera If this list is empty, the camera node will not properly synchronize them 
 // Master camera node will wait for other cameras to be online and will send hardware synchronization signal 
--- driver_code/dv-ros-master/script/run.sh
+++ driver_code/dv-ros-master/script/run.sh
@@ -4,5 +4,5 @@ sleep 1s
 
 # 可视化
 gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization left_event_visualization.launch;exec bash"'
-sleep 10s
+# sleep 10s
 gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization right_event_visualization.launch;exec bash"'
