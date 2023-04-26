7bbac5157b56fa4546b0cd343d837e5852dc6e31
KwanWaiPang <gwpscut@163.com>
2023-04-26 19:47:44 +0800

    synchronization for stereo event cameras

--- driver_code/dv-ros-master/script/run.sh
+++ driver_code/dv-ros-master/script/run.sh
@@ -3,4 +3,6 @@ gnome-terminal --tab -e 'bash -c "roscore;exec bash"'
 sleep 1s
 
 # 可视化
-gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization event_visualization.launch;exec bash"'
+gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization left_event_visualization.launch;exec bash"'
+sleep 10s
+gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization right_event_visualization.launch;exec bash"'
