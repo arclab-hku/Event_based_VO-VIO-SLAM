eb5a0a60635748a057d90ad49e34fa9f33d23361
KwanWaiPang <gwpscut@163.com>
2023-06-02 13:54:40 +0800

    polyu_driving

--- driver_code/dv-ros-master/script/run.sh
+++ driver_code/dv-ros-master/script/run.sh
@@ -2,19 +2,30 @@
 gnome-terminal --tab -e 'bash -c "roscore;exec bash"'
 sleep 1s
 
-# davis346
-gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization left_davis346.launch;exec bash"'
+# # # davis346
+gnome-terminal --tab -e 'bash -c "roslaunch dv_ros_visualization left_davis346.launch;exec bash"'
 # sleep 10s
-gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization right_davis346.launch;exec bash"'
+gnome-terminal --tab -e 'bash -c "roslaunch dv_ros_visualization right_davis346.launch;exec bash"'
 
 # # # dvxplorer
-gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization left_dvxplorer.launch;exec bash"'
+gnome-terminal --tab -e 'bash -c "roslaunch dv_ros_visualization left_dvxplorer.launch;exec bash"'
 # # # sleep 10s
-gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization right_dvxplorer.launch;exec bash"'
+gnome-terminal --tab -e 'bash -c "roslaunch dv_ros_visualization right_dvxplorer.launch;exec bash"'
 
 
 # 可视化
-gnome-terminal --window -e 'bash -c "roslaunch dv_ros_visualization rqt_visualization.launch;exec bash"'
+gnome-terminal --tab -e 'bash -c "roslaunch dv_ros_visualization rqt_visualization.launch;exec bash"'
+
+# 录制osbag
+# roslaunch dv_ros_visualization event_record.launch 
+# 同步机载电脑
+# sudo ptpd -g -i eno1 -C
+
+
+# gnome-terminal --tab -e 'bash -c "roslaunch dv_ros_visualization event_visualization.launch;exec bash"'
+# gnome-terminal --window -e 'bash -c "rosbag play -r 1.0 --pause ~/road_data_bag_event_last.bag;exec bash"'
+# gnome-terminal --window -e 'bash -c "rostopic echo /DAVIS346_left/image/header;exec bash"'
+# gnome-terminal --window -e 'bash -c "rostopic echo /DAVIS346_right/image/header;exec bash"'
 
 #bag record
 sleep 6s
